f7f476f933a2b77ac77b28bcffb5b942fb8c709d
ashferoz <ash.safrina@gmail.com>
2024-06-20 10:53:34 +0800

    Add horizontal scrolling in user page.

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import UserGoingEventCard from "./UserGoingEventCard";
 import UserInterestedEventCard from "./UserInterestedEventCard";
 import ReviewCard from "./ReviewCard";
+import styles from "./css/UserPage.module.css";
 
 const UserPage = (props) => {
   useEffect(() => {
@@ -23,42 +24,49 @@ const UserPage = (props) => {
     <>
       <h1>Hello Ash.</h1>
       <hr />
+      <br />
 
       <div className="container">
-        <h3>Your upcoming events</h3>
-        {goingEvents.map((item) => (
-          <UserGoingEventCard
-            key={item.id}
-            band={item.fields.band}
-            date={item.fields.date}
-            time={item.fields.time}
-            venue={item.fields.venue}
-          />
-        ))}
+        <h3 className={styles.sectionTitles}>Your upcoming events</h3>
+        <div className={styles.box}>
+          {goingEvents.map((item) => (
+            <UserGoingEventCard
+              key={item.id}
+              band={item.fields.band}
+              date={item.fields.date}
+              time={item.fields.time}
+              venue={item.fields.venue}
+            />
+          ))}
+        </div>
       </div>
 
       <div className="container">
         <h3>Interested in</h3>
-        {interestedEvents.map((item) => (
-          <UserInterestedEventCard
-            key={item.id}
-            band={item.fields.band}
-            date={item.fields.date}
-            time={item.fields.time}
-            venue={item.fields.venue}
-          />
-        ))}
+        <div className={styles.box}>
+          {interestedEvents.map((item) => (
+            <UserInterestedEventCard
+              key={item.id}
+              band={item.fields.band}
+              date={item.fields.date}
+              time={item.fields.time}
+              venue={item.fields.venue}
+            />
+          ))}
+        </div>
       </div>
 
       <div className="container">
         <h3>Reviews of past events</h3>
-        {pastEvents.map((item) => (
-          <ReviewCard
-            key={item.id}
-            band={item.fields.band}
-            review={item.fields.review}
-          />
-        ))}
+        <div className={styles.box}>
+          {pastEvents.map((item) => (
+            <ReviewCard
+              key={item.id}
+              band={item.fields.band}
+              review={item.fields.review}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
